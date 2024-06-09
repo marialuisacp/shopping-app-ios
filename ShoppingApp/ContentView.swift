78ee0823920015a019fba4ea8b752f6e343efe70
Maria Luísa Costa Pinto <mariacosta.thoughtworks@latam.com>
2024-06-09 16:55:49 -0300

    feat: creating detail screen style

--- ShoppingApp/ContentView.swift
+++ ShoppingApp/ContentView.swift
@@ -32,10 +32,20 @@ struct ContentView: View {
                         NavigationLink {
                             
                             VStack {
+                                AsyncImage(url: URL(string: product.imageUrl)) { image in
+                                    image.resizable().aspectRatio(contentMode: .fit)
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: .infinity, height: 180)
+                                .background(Rectangle().fill(Color.white))
                                 VStack(alignment: .leading) {
-                                    Text(product.title)
-                                        .font(.largeTitle)
-                                    Text("Test")
+                                    Text(product.title).foregroundStyle(Color("primary"))
+                                        .font(.title)
+                                    Text("$ " + String(format: "%.2f", product.price)).font(.headline)
+                                    Divider().background(Color.black).padding(.trailing, 128)
+                                    Text(product.categoty).font(.caption).foregroundStyle(Color("secondary"))
+                                    Text(product.description).font(.caption)
                                 }
                                 .padding(.horizontal, 24)
                                 .padding(.bottom, 64)
