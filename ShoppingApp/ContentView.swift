80806688d716aa27337f358ffe3e2e1d1fdbcb74
Maria Luísa Costa Pinto <contato@malu.dev>
2024-06-09 15:05:58 -0300

    feat: separate in service

--- ShoppingApp/ContentView.swift
+++ ShoppingApp/ContentView.swift
@@ -2,12 +2,6 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
     @State var productsArray: [Product] = [Product]()
     
     var numbers = [0, 1, 2, 3]
@@ -42,91 +36,22 @@ struct ContentView: View {
                         }
                 }
             }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
             .listStyle(GroupedListStyle())
-            
-            Text("Select an item")
         }
         .task {
-            guard let url = URL(string: "https://fakestoreapi.com/products") else {
-                return
-            }
-            _ = URLRequest(url: url)
-            do {
-                let (data, _) = try await URLSession.shared.data(from: url)
-                        do {
-                            print("Shopping Service:: serialize")
-                            
-                            if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [NSObject] {
-                                for product in json {
-                                    let newProduct = Product(id: product.value(forKey: "id") as! Int,
-                                                             categoty: product.value(forKey: "category") as! String,
-                                                             imageUrl:  product.value(forKey: "image") as! String,
-                                                             price: product.value(forKey: "price") as! Double,
-                                                             title: product.value(forKey: "title") as! String,
-                                                             description: product.value(forKey: "description") as! String)
-                                    
-                                    productsArray.append(newProduct)
-                                }
-                            }
-                        } catch let error {
-                            print("Shopping Service:: error on get produtcs")
-                            print(error)
-                        }
-                
-            } catch {
-                print("Failed to fetch site.")
-                
+            await Shoppingservice.getProducts {
+                products in 
+                for product in products {
+                    let newProduct = Product(id: product.value(forKey: "id") as! Int,
+                                             categoty: product.value(forKey: "category") as! String,
+                                             imageUrl:  product.value(forKey: "image") as! String,
+                                             price: product.value(forKey: "price") as! Double,
+                                             title: product.value(forKey: "title") as! String,
+                                             description: product.value(forKey: "description") as! String)
+
+                    productsArray.append(newProduct)
+                }
             }
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
--- ShoppingApp/services/ShoppingService.swift
+++ ShoppingApp/services/ShoppingService.swift
@@ -1,38 +1,25 @@
 import Foundation
 
 class Shoppingservice {
-    class func getProducts() {
-        let url = URL(string: "https://fakestoreapi.com/products")!
-        let session = URLSession.shared
-        let request = URLRequest(url: url)
-        var productsArray: [NSObject] = [NSObject]()
-
-        print("Shopping Service:: get data")
-        let task = try session.dataTask(
-            with: request as URLRequest,
-            completionHandler: { 
-                data, response, error in
-                    guard error == nil else {
-                        return
-                    }
-                    guard let data = data else {
-                        return
-                    }
-                do {
-                    print("Shopping Service:: serialize")
-                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [NSObject] {
-                        productsArray = json
-                        print("Shopping Service:: save products")
-                        print(productsArray)
-                    }
-                } catch let error {
-                    print("Shopping Service:: error on get produtcs")
-                    print(error.localizedDescription)
+    class func getProducts(completionHandler: (_ products: [NSObject]) -> Void) async {
+        guard let url = URL(string: "https://fakestoreapi.com/products") else {
+            return
+        }
+        _ = URLRequest(url: url)
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            do {
+                print("Shopping Service:: serialize")
+                
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [NSObject] {
+                    completionHandler(json)
                 }
-        })
-        
-        task.resume()
-        print(productsArray)
-        print("Shopping Service:: complete request")
+            } catch let error {
+                print(error)
+            }
+        } catch {
+            print("Failed to fetch site.")
+        }
     }
+        
 }
