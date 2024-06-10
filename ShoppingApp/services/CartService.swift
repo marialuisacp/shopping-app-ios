2859b53384f9ee9aa44f7cf19b879a3ba6025191
Maria Luísa Costa Pinto <mariacosta.thoughtworks@latam.com>
2024-06-10 20:39:26 -0300

    feat: adding cart icon

---
+++ ShoppingApp/services/CartService.swift
@@ -0,0 +1,38 @@
+import CoreData
+
+struct CartService {
+    static let shared = CartService()
+
+    static var preview: CartService = {
+        let result = CartService(inMemory: true)
+        let viewContext = result.container.viewContext
+
+        for _ in 0..<10 {
+            var newProduct = CartItem(context: viewContext)
+        }
+        
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "ShoppingApp")
+
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
