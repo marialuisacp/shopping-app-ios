2ea29658997b38389588c8d1998a1519df3e6f80
Maria Luísa Costa Pinto <contato@malu.dev>
2024-06-11 01:25:19 -0300

    feat: adding count cart

--- ShoppingApp/components/ShoppingHeader.swift
+++ ShoppingApp/components/ShoppingHeader.swift
@@ -1,6 +1,12 @@
 import SwiftUI
 
 struct ShoppingHeader: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \CartItem.timestamp, ascending: true)],
+        animation: .default)
+    private var cart: FetchedResults<CartItem>
+    
     var body: some View {
         Image("degrade")
             .resizable()
@@ -17,13 +23,26 @@ struct ShoppingHeader: View {
                     CartView()
                 } label: {
                     VStack {
-                        Image("cart")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 24, height: 24)
+                        Text(String(cart.endIndex))
+                            .font(.caption.bold())
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .background(
+                                Circle()
+                                    .fill(Color("purple"))
+                                    .frame(width: 20, height: 20))
+                            .padding(.bottom, -16)
+                            .padding(.trailing, -16)
+                            .zIndex(1)
+                        VStack(alignment: .center) {
+                            Image("cart")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 24, height: 24)
+                        }
+                        .frame(width: 40, height: 40)
+                        .background(RoundedRectangle(cornerRadius: 8).fill(Color(.sRGBLinear, white: 1, opacity: 0.1)))
                     }
-                    .frame(width: 40, height: 40)
-                    .background(RoundedRectangle(cornerRadius: 8).fill(Color(.sRGBLinear, white: 1, opacity: 0.1)))
                 }
             }
             Text("Your one-stop online shop for all your needs!")
